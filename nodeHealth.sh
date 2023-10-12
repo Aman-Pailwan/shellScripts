9d546d4ddb60602957ee7afc040452372eed43c6
Aman-Pailwan <aman.pailwan.773@gmail.com>
2023-10-12 12:47:11 +0530

    First Commit

---
+++ nodeHealth.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+# Author : Aman
+# This Script is used for understanding the health of a node
+
+
+
+echo "Displaying the Disk Space of Node:- "
+df -h
+
+echo "Displaying the free memory:- "
+free -g
+
+echo "Displaying the number of CPU :- "
+nproc
